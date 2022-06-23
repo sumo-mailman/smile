4b5f6137cbf532e5f3b821097df1a9bc65b338f5
Josh <joshlim092@gmail.com>
2022-06-23 11:54:21 +0100

    Controller for home is now owrking

--- src/main/java/com/example/demo/smile/SmilesController.java
+++ src/main/java/com/example/demo/smile/SmilesController.java
@@ -1,5 +1,13 @@
 package com.example.demo.smile;
 
-public class SmilesController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class SmilesController {
+  @GetMapping("/home")
+  public Smile create() {
+    Smile smile = new Smile(1, true, true);
+    return smile;
+  }
 }
