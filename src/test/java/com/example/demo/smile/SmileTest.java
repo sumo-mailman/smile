6a751d51c4b804d3a8341e591d08a3cf27330707
Josh <joshlim092@gmail.com>
2022-06-23 13:02:31 +0100

    for testing our GitHub Actions

--- src/test/java/com/example/demo/smile/SmileTest.java
+++ src/test/java/com/example/demo/smile/SmileTest.java
@@ -9,7 +9,7 @@ class SmileTest {
   @Test
   void testConstructors() {
     // Long and Boolean
-    Smile subject = new Smile(1, false, false);
+    Smile subject = new Smile(1, true, false);
     assertEquals(1, subject.getId());
     assertEquals(true, subject.isCrying());
     assertEquals(false, subject.isLaughing());
